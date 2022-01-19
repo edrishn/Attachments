e4a74c4632d19a8b746c0a225e29aab075352fb6
edrishn <edrishn@gmail.com>
2022-01-19 10:12:54 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AttachmentsDialog from "./AttachmentsDialog";
 export default function App() {
   let serverURL = "https://37ac4910-074b-4faf-bde8-c37c90795f6b.mock.pstmn.io";
   return (
+    //Test
     <div className="App">
       <AttachmentsDialog serverUrl={serverURL} queryString="Product/1" />
     </div>
